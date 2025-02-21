1fdb3ba61841407ccc8ccb6304ba92b6a91abbfc
amornthep.c <amornthep.c@systemweb.co.th>
2025-02-21 13:53:26 +0700

    Design Database and Seeding Data

---
+++ Retail.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Retail.Application.Database;
+using System.Runtime.CompilerServices;
+
+namespace Retail.Application
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton<IDbConnectionFactory>(_ => new SqlServerConnectionFactory(connectionString));
+            services.AddSingleton<DbInitializer>();
+
+            return services;
+        }
+    }
+}
---
+++ Retail.Application/Database/IDbConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Retail.Application.Database
+{
+    public interface IDbConnectionFactory
+    {
+        Task<IDbConnection> CreateConnectionAsync(CancellationToken token = default);
+    }
+}
